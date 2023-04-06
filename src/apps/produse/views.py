2315af0280f4cf2cff20b623b5e8508c51f848b7
luciansacota <lucian.sacota@gmail.com>
2023-04-06 10:23:24 +0300

    adaugare dummy data

--- src/apps/produse/views.py
+++ src/apps/produse/views.py
@@ -3,8 +3,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+products = [
+    {
+        'id':1,
+        'nume':'paine',
+        'cumparat':True,
+    },
+    
+    {
+        'id':2,
+        'nume':'lapte',
+        'cumparat':False,
+    },
+    
+    {
+        'id':3,
+        'nume':'oua',
+        'cumparat':False,
+    },
+]
+
 def produse(request):
     context = {
-        
+        'products':products
     }
     return render(request, 'produse/produse.html', context)
